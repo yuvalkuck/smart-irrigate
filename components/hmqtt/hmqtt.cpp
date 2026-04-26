72c8759dc38178472cbd85cfb876874e996763f5
Yuval Kuck <kuckng@gmail.com>
2026-04-26 19:20:47 +0300

    sub

--- components/hmqtt/hmqtt.cpp
+++ components/hmqtt/hmqtt.cpp
@@ -25,17 +25,17 @@ static void mqttEventHandler(void* handler_args, esp_event_base_t base, int32_t 
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+        msg_id = esp_mqtt_client_publish(client, "/client/upstream", "{}", 0, 1, 0);
         ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
 
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+        msg_id = esp_mqtt_client_subscribe(client, "/client/configuration", 0);
         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        // msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+        // ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        //
+        // msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+        // ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
@@ -43,8 +43,8 @@ static void mqttEventHandler(void* handler_args, esp_event_base_t base, int32_t 
 
     case MQTT_EVENT_SUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        // msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        // ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
--- test/mosquitto/config/create_users.sh
+++ test/mosquitto/config/create_users.sh
@@ -1,6 +1,16 @@
-#/bin/bash
-rm password.txt
-docker exec -ti mqtt mosquitto_passwd -c -b /mosquitto/config/password.txt admin admin
-docker exec -ti mqtt mosquitto_passwd -b /mosquitto/config/password.txt client client
-docker exec -ti mqtt mosquitto_passwd -b /mosquitto/config/password.txt test test
+#/bin/bash -x
+dexec="docker exec -ti mqtt"
+\rm password.txt
+touch password.txt
+chmod 700 password.txt
+# create users
+${dexec} mosquitto_passwd -b /mosquitto/config/password.txt admin admin
+${dexec} mosquitto_passwd -b /mosquitto/config/password.txt client client
+${dexec} mosquitto_passwd -b /mosquitto/config/password.txt test test
+# public topic
+${dexec} mosquitto_pub -u admin -P admin -t "client/configuration" -m ""
+${dexec} mosquitto_pub -u admin -P admin -t "client/upstream" -m ""
+# sub
+echo "${dexec} mosquitto_sub -u admin -P admin -t \"client/upstream\""
+#restart
 docker restart mqtt
