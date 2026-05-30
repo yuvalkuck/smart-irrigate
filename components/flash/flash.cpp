384e8127c06ef578b172527ca671142c95fd4139
Yuval Kuck <kuckng@gmail.com>
2026-05-30 16:13:46 +0300

    cleanup

--- components/flash/flash.cpp
+++ components/flash/flash.cpp
@@ -1,7 +1,7 @@
-#include "nvs_flash.h"
 #include "esp_log.h"
 #include "flash.h"
 #include "sdkconfig.h"
+#include "nvs_flash.h"
 
 #define NVS_NAMESPACE "."
 #define NVS_PARTITION_NAME "config"
@@ -10,22 +10,23 @@ static const char* TAG = "flash:";
 static nvs_handle_t hNVS;
 esp_err_t nvs_set_str(uint32_t handle, const char* key, int value, size_t* len);
 
-static void nvsCreateKeyStr(const char *name, const char *defaule_value = "") {
-    ESP_LOGI(TAG, "%s:%s", __func__,name);
+static void nvsCreateKeyStr(const char* name, const char* defaule_value = "") {
+    ESP_LOGI(TAG, "%s:%s", __func__, name);
     esp_err_t err;
     size_t len = 0;
-    char tmp[64]={0};
+    char tmp[64] = {0};
     // Step 1: Get the size of the stored string (includes null terminator)
     err = nvs_get_str(hNVS, name, tmp, &len);
     if (err == ESP_ERR_NVS_NOT_FOUND) {
         err = nvs_set_str(hNVS, name, defaule_value);
         if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) {
-            ESP_LOGE(TAG, "failed to create: %s",defaule_value);
+            ESP_LOGE(TAG, "failed to create: %s", defaule_value);
         }
     }
 }
-static void nvsCreateKeyInt(const char *name, long defaule_value = 0) {
-    ESP_LOGI(TAG, "%s:%s", __func__,name);
+
+static void nvsCreateKeyInt(const char* name, long defaule_value = 0) {
+    ESP_LOGI(TAG, "%s:%s", __func__, name);
     esp_err_t err;
     int32_t value;
     // Step 1: Get the size of the stored string (includes null terminator)
@@ -36,6 +37,14 @@ static void nvsCreateKeyInt(const char *name, long defaule_value = 0) {
             ESP_LOGE(TAG, "failed to create: %d", defaule_value);
         }
     }
+}
+
+esp_err_t nvs_config(nvs_handle_t* handler) {
+    esp_err_t ret = nvs_open_from_partition(NVS_PARTITION_NAME,NVS_NAMESPACE, NVS_READWRITE, handler);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "failed (%d) to open partition: %s:%s", ret, NVS_PARTITION_NAME, NVS_NAMESPACE);
+    }
+    return ret;
 }
 
 esp_err_t init_flash(void) {
@@ -50,9 +59,9 @@ esp_err_t init_flash(void) {
         ESP_ERROR_CHECK(nvs_flash_erase_partition(NVS_PARTITION_NAME));
         nvs_flash_init_partition(NVS_PARTITION_NAME);
     }
-    ret = nvs_open_from_partition(NVS_PARTITION_NAME,NVS_NAMESPACE, NVS_READWRITE, &hNVS);
+    ret = nvs_config(&hNVS);
     if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "failed (%d) to open partition: %s:%s",ret, NVS_PARTITION_NAME,NVS_NAMESPACE);
+        ESP_LOGE(TAG, "failed (%d) to open partition: %s:%s", ret, NVS_PARTITION_NAME, NVS_NAMESPACE);
         return 0;
     }
     ESP_LOGI(TAG, "start init keys");
@@ -66,7 +75,7 @@ esp_err_t init_flash(void) {
     nvsCreateKeyStr("locale_tz",CONFIG_DEFAULT_LOCALE_TIME_ZONE);
     ret = nvs_commit(hNVS);
     if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "failed (%d) nvs_commit",ret);
+        ESP_LOGE(TAG, "failed (%d) nvs_commit", ret);
     }
     nvs_close(hNVS);
     return ESP_OK;
--- components/flash/include/flash.h
+++ components/flash/include/flash.h
@@ -1,2 +1,3 @@
 #include "esp_err.h"
 esp_err_t init_flash(void);
+esp_err_t nvs_config( uint32_t *);
