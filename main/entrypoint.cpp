604cccd01024e806b76ed341df9403f09eb09b2b
Yuval Kuck <kuckng@gmail.com>
2026-06-01 18:14:05 +0300

    add key force load in BLE mode

--- main/entrypoint.cpp
+++ main/entrypoint.cpp
@@ -18,7 +18,8 @@
 
 static const char* TAG = "App:";
 extern bme680_values_float_t telemetryValues;
-#define GPIO_LED 15
+#define GPIO_LED GPIO_NUM_15
+#define GPIO_FORCE_BLE GPIO_NUM_17
 ESP_EVENT_DEFINE_BASE(COMMON_BASE_EVENTS);
 
 static void setLedState(int fliper) {
@@ -33,6 +34,7 @@ void continue_after_time_sync_cb(struct timeval* tv) {
     ESP_LOGI(TAG, "set TimeZone to: %s", CONFIG_DEFAULT_LOCALE_TIME_ZONE);
     start_hmqtt();
 }
+
 static void cbCommonEventHandler(void* handler_args, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     ESP_LOGV(TAG, "%s", __func__);
     if (event_base == COMMON_BASE_EVENTS) {
@@ -47,38 +49,54 @@ static void cbCommonEventHandler(void* handler_args, esp_event_base_t event_base
                 break;
         }
     }
-
 }
-
-extern "C" void app_main(void) {
-    ESP_LOGI(TAG, "setting up");
-
+static void init_gpio() {
     gpio_reset_pin((gpio_num_t)GPIO_LED);
     gpio_set_direction((gpio_num_t)GPIO_LED, GPIO_MODE_OUTPUT);
-
+    gpio_reset_pin((gpio_num_t)GPIO_FORCE_BLE);
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << GPIO_FORCE_BLE),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&io_conf);
+    vTaskDelay(pdMS_TO_TICKS(50)); // create small delay
+}
+extern "C" void app_main(void) {
+    ESP_LOGI(TAG, "setting up");
+    init_gpio();
     //Initialize NVS
+    bool initRegular = (gpio_get_level(GPIO_FORCE_BLE) == 0 ? false : true);
     esp_err_t ret = init_flash();
+
     ESP_ERROR_CHECK(ret);
     ESP_LOGI(TAG, "start event loop default");
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    setLedState(true);
     // test if we have wifi config
     { // block to release hCfg when not use anymore
+
         NvsConfig hCfg;
-        if ( !hCfg ) {
+        if (!hCfg) {
             return;
         }
-        if ( hCfg.isStrEmpty(CFG_NVS_KEY_WIFI_SSID) ) {
+        if (!initRegular || hCfg.isStrEmpty(CFG_NVS_KEY_WIFI_SSID)) {
             ESP_LOGI(TAG, "Start BLE");
             init_blesrv();
             start_blesrv();
-        } else {
+            initRegular = false;
+        }
+        else {
             ESP_LOGI(TAG, "Start Regular Load");
             esp_event_handler_register(
-                    COMMON_BASE_EVENTS,          // Event base
-                    ESP_EVENT_ANY_ID,            // Event ID (or look for a specific one like CUSTOM_EVENT_SENSOR_READY)
-                    &cbCommonEventHandler,   // The callback function pointer
-                    NULL                         // Optional arguments passed to handler_args
-                );
+                COMMON_BASE_EVENTS, // Event base
+                ESP_EVENT_ANY_ID, // Event ID (or look for a specific one like CUSTOM_EVENT_SENSOR_READY)
+                &cbCommonEventHandler, // The callback function pointer
+                NULL // Optional arguments passed to handler_args
+            );
             init_telemetry();
 
             init_wifi();
@@ -94,14 +112,15 @@ extern "C" void app_main(void) {
     //
     ESP_LOGI(TAG, "working stage");
     //
-    int ledFlip = 0;
+    auto ticker = (initRegular ? 2000 : 250);
+    int ledFlip = 1;
     for (;;) {
         --ledFlip;
         if (ledFlip < 0) {
             ledFlip = 1;
         }
         setLedState(ledFlip);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(ticker / portTICK_PERIOD_MS);
     }
     // if (CONFIG_LOG_MAXIMUM_LEVEL > CONFIG_LOG_DEFAULT_LEVEL) {
     //     /* If you only want to open more logs in the wifi module, you need to make the max level greater than the default level,
